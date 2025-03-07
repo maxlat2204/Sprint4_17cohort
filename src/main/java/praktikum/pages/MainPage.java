022dbe7712234f3bef30071157e3d1e650d96c66
Maksim Latyshev <maxlat2013@yandex.ru>
2025-03-07 22:53:39 +0300

    add LocatroMainPage

--- src/main/java/praktikum/pages/MainPage.java
+++ src/main/java/praktikum/pages/MainPage.java
@@ -1,4 +1,29 @@
 package praktikum.pages;
 
+import org.openqa.selenium.By;
+
 public class MainPage {
+
+    //Верхняя кнопка заказать
+    protected final By upOrderButton = By.xpath(".//div[@class='Header_Nav__AGCXC']/button[@class='Button_Button__ra12g']");
+
+    //Нижняя кнопка Заказать
+    protected final By downOrderButton = By.className("Button_Middle__1CSJM");
+
+    //Кнопка Статус Заказа
+    protected final By statusOrderButton = By.className("Header_Link__1TAG7");
+
+    //Кнопка Go! в статусе заказа
+    protected final By goButton = By.className("Header_Button__28dPO");
+
+    //Поле ввода Заказа
+    protected final By orderFieldInput = By.className("Input_Input__1iN_Z ");
+
+    //Кнопка самокат в Шапке
+    protected final By scooterButton = By.xpath(".//a[@class='Header_LogoScooter__3lsAR']/img[@alt='Scooter']");
+
+    //Кнопка Яндекс в шапке
+    protected final By yandexBuuton = By.xpath(".//img[@alt='Yandex']");
+
+
 }
